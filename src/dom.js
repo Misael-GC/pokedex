cde634471d52b6fb54c1712344d3d094780c3f07
Misael-GC <misael.gomez1623@gmail.com>
2022-08-14 18:02:11 -0500

    archivos segunda parte

---
+++ src/dom.js
@@ -0,0 +1,17 @@
+const pokebolaOpen = document.querySelector('.pokebola-prymary');
+const tapaPokedex = document.querySelector('.tapa');
+const pokeLink = document.querySelector('.pokebola');
+const pokeClose = document.querySelector('.pokebola-link');
+
+
+pokebolaOpen.addEventListener('click', toggleOpenPokedex);
+pokeClose.addEventListener('click', toggleClosePokedex)
+
+function toggleOpenPokedex(){
+    const toggle = tapaPokedex.classList.toggle('inactive');
+    const apperPokebolaLink = pokeLink.classList.remove('inactive');
+}
+
+function toggleClosePokedex(){
+    const inactivePokedex = tapaPokedex.classList.remove('inactive');
+}
---
+++ src/index.js
@@ -0,0 +1,92 @@
+const pokeImg = document.querySelector('#poke-img');
+const pokeDescription = document.querySelector('#desc')
+const pokeDmg = document.querySelector('#dmg');//owo
+const pokeDef = document.querySelector('#dfs');;//:0
+const pokeSpeed = document.querySelector("#speed");
+const pokeSpDef = document.querySelector("#sp-def"); //:l
+const pokeSpAtck = document.querySelector("#sp-atck");
+const pokePS = document.querySelector("#ps"); //:o
+const pokemonsList = document.querySelector("#pokemons");
+const pokeEvol = document.querySelector("#evol");
+const pokeName = document.querySelector('#name');
+const BASE_API = 'https://pokeapi.co/api/v2/';
+const pokemon_API = `${BASE_API}pokemon`;
+let currentPokemon; //uwu
+let sprites = []; //80 va a guardar todos los valores de las img y se´r más facil iterar
+let currentSprite = 0;//90
+
+const fetchData = (API) => {
+    return  fetch(API)
+        .then(res => res.json())
+        .then(data => data)
+};
+
+const writeDescription = (API) => {
+    fetchData(API).then((specie) => {
+      pokeDescription.textContent = specie.flavor_text_entries[0].flavor_text
+    });
+  };
+
+// fetchData(`${pokemon_API}/1`)
+//vamos a mostrarlo en nuestro HTML
+const printPokemon = (pokemon) => {
+    const data = fetchData(`${pokemon_API}/${pokemon}`).then(data =>{
+        if(sprites.length > 0){
+            sprites = []
+        }
+        currentPokemon = data; //uwu
+        console.log(currentPokemon);
+        pokeImg.src = data.sprites.front_default;
+        pokePS.textContent = data.stats[0].base_stat; //:o
+        pokePS.style.width = `${data.stats[0].base_stat/2}%`;
+        pokeDmg.textContent = data.stats[1].base_stat;//owo
+        pokeDmg.style.width = `${data.stats[1].base_stat/2}%`;
+        pokeDef.textContent = data.stats[2].base_stat;//:0
+        pokeDef.style.width = `${data.stats[2].base_stat/2}%`;//:0
+        pokeSpDef.textContent = data.stats[4].base_stat;
+        pokeSpDef.style.width =`${data.stats[4].base_stat/2}%`;
+        pokeSpeed.textContent = data.stats[5].base_stat;
+        pokeSpeed.style.width =  `${data.stats[5].base_stat/2}%`;
+        writeDescription(data.species.url)
+        pokeName.textContent = data.name;
+        const pokeSprites = currentPokemon.sprites//80
+        for (const key in pokeSprites) {//80
+            if(typeof pokeSprites[key] === "string")
+                sprites.push(pokeSprites[key]);
+        }
+        console.log(sprites);
+    });
+    // console.log(data)
+};
+
+const prevImg = () =>{
+    // pokeImg.src = currentPokemon.sprites.back_default;
+    if(currentSprite === 0){ //90 si estamos en el limite hacia atras, tenemos que volver a la pposition más grande del array
+        currentSprite = sprites.length - 1;// le restamos 1 porque esta API  no cuenta el cero
+    }else{//si es > 0 
+        currentSprite --
+    }
+    pokeImg.src = sprites[currentSprite];
+}
+
+const nextImg = () =>{
+    if(currentSprite === sprites.length - 1){ //90 
+        currentSprite = 0;// le restamos 1 porque esta API  no cuenta el cero
+    }else{//si es > 0 
+        currentSprite ++
+    }
+    pokeImg.src = sprites[currentSprite];
+}
+
+const prevPokemon = () => {
+    if(currentPokemon.id === 1){
+        currentPokemon.id = 250;
+    }
+    printPokemon(currentPokemon.id - 1)
+}
+
+const nextPokemon = () => {
+    printPokemon(currentPokemon.id + 1)
+}
+
+printPokemon(1)
